f0cd01e62e68c99e9b3811483f73700851f32de1
Sanjeevikumar038 <sanjeevikumarwk@gmail.com>
2026-09-24 20:58:22 +0530

    QR session time conflict fixed

--- eduflow-backend/src/main/java/com/eduflow/EduflowBackendApplication.java
+++ eduflow-backend/src/main/java/com/eduflow/EduflowBackendApplication.java
@@ -2,9 +2,18 @@ package com.eduflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class EduflowBackendApplication {
+
+    @PostConstruct
+    public void init() {
+        // Force the JVM to use Indian Standard Time (IST) 
+        // to prevent time discrepancies when deployed to Render (UTC)
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(EduflowBackendApplication.class, args);
